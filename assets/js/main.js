44792a4136077cf7f239e8bf11618cfe214c169f
nickerick <nickerickson737@gmail.com>
2022-04-15 14:19:41 -0500

    updated js

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+const button1 = document.getElementById('test1');
+const docColor = document.getElementById('proj1');
+
+// console.log(button1);
+button1.addEventListener('click', () => {
+    // console.log(docColor);
+    docColor.style.color = 'green';
+});
