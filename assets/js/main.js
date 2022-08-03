3e33a8084fde2a74de451275a16b9700bb1f388f
nickerick <nickerickson737@gmail.com>
2022-08-02 22:43:04 -0500

    fixes colorchanger on exp

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,6 +5,7 @@ button1.addEventListener('click', () => {
     var yellowElement2 = document.querySelectorAll("[id='proj2']");
     const yellowElement3 = document.getElementById('titleSpan');
     var yellowElement4 = document.querySelectorAll("h1");
+    var yellowElement5 = document.getElementById("expheader");
 
     yellowElement1.style.animation = 'color-change 1s infinite';
     for(var i = 0; i < yellowElement2.length; i++) 
@@ -12,5 +13,6 @@ button1.addEventListener('click', () => {
     yellowElement3.style.animation = 'color-change 1s infinite';
     for(var i = 0; i < yellowElement4.length; i++) 
         yellowElement4[i].style.animation='color-change 1s infinite';
+    yellowElement5.style.animation = 'color-change 1s infinite';
 });
 
